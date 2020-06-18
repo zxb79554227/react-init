dc16edbb6a61c8f4c96b904d90188804bc402d07
Hanfeng <hanfengwebdesign@gmail.com>
2020-06-18 19:22:10 +0800

    2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'semantic-ui-less/semantic.less'
-import './index.css';
+import "semantic-ui-css/semantic.min.css";
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
